17cb7620d222e31e5cf4b974a7cbaab91ed72b30
matssteinweg <mats.steinweg@me.com>
2020-08-13 17:43:59 +0200

    Change return value of predict function

--- particle_filter.m
+++ particle_filter.m
@@ -1,16 +1,17 @@
 % This function performs one iteration of the particle filter.
 % Inputs:
-%           S(t-1)                    4xM
-%           u                         3XM
+%           S(t-1)                    4XM
+%           v                         1X1
+%           omega                     1X1
 %           z                         2Xn
 %           association_ground_truth  1Xn
 % Outputs:
-%           S(t)                      4xM
+%           S(t)                      4XM
 %           measurement_info          1Xn
-function [S, measurement_info] = particle_filter(S, u, z, association_ground_truth)
+function [S, measurement_info] = particle_filter(S, v, omega, delta_t, z, association_ground_truth)
 
 % set simulation mode
-global KNOWN_ASSOCIATIONS % use association ground truth or perform ML data association
+global DATA_ASSOCIATION % use association ground truth or perform ML data association
 global RESAMPLE_MODE % re-sampling strategy for weighted particles
 
 % import global variables
@@ -25,21 +26,24 @@ n_measurements = size(z, 2);
 measurement_info = zeros(1, n_measurements);
 
 % predict step including particle diffusion
-[S_bar] = predict(S, u);
+[S_bar] = predict(S, v, omega, delta_t);
 
-% ML data association or ground truth information
-if KNOWN_ASSOCIATIONS
-    [outlier, Psi, c] = associate(S_bar, z, association_ground_truth);
-else
-    [outlier, Psi, c] = associate(S_bar, z);
-end
+% check if measurements are available
+if n_measurements > 0
+    
+    % ML data association or ground truth information
+    if strcmp(DATA_ASSOCIATION, 'Off')
+        [outlier, Psi, c] = associate(S_bar, z, association_ground_truth);
+    else
+        [outlier, Psi, c] = associate(S_bar, z);
+    end
 
-% print number of outliers detected
-if sum(outlier)
-        fprintf('warning, %d/%d measurements were labeled as outliers, t=%d\n', sum(outlier), n_measurements, t);
-end
+    % print number of outliers detected
+    if sum(outlier)
+            fprintf('warning, %d/%d measurements were labeled as outliers, t=%d\n', sum(outlier), n_measurements, t);
+    end
 
-% store information about measurements
+    % store information about measurements
     for i = 1 :n_measurements
         associated_landmark = landmark_ids(mode(c(1, i, :))); % associated landmark for majority of particles
         if association_ground_truth(i) ~= associated_landmark && outlier(i) == 0
@@ -50,17 +54,23 @@ end
             measurement_info(i) = 2; % outlier
         end
     end
-    
-% weight particles
-S_bar = weight(S_bar, Psi, outlier);
-
-% resample particles
-switch RESAMPLE_MODE
-    case 0
-        S = S_bar;
-    case 1
-        S = multinomial_resample(S_bar);
-    case 2
-        S = systematic_resample(S_bar);
+        
+    % weight particles
+    S_bar = weight(S_bar, Psi, outlier);
+        
+    % resample particles
+    switch RESAMPLE_MODE
+        case 0
+            S = S_bar;
+        case 1
+            S = multinomial_resample(S_bar);
+        case 2
+            S = systematic_resample(S_bar);
+    end
+else
+    % use particle set after prediction as final particle set in case
+    % there's no measurements available
+    S = S_bar;
+    measurement_info = [];
 end
 end
--- predict.m
+++ predict.m
@@ -1,14 +1,25 @@
 % This function performs the prediction step.
 % Inputs:
 %           S(t-1)            4XN
-%           u(t)              3X1
+%           v                 1X1
+%           omega             1X1
 % Outputs:   
 %           S_bar(t)          4XN
-function [S_bar] = predict(S, u)
+function [S_bar] = predict(S, v, omega, delta_t)
+
+    % Comment out any S_bar(3, :) = mod(S_bar(3,:)+pi,2*pi) - pi before
+    % running the test script as it will cause a conflict with the test
+    % function. If your function passes, uncomment again for the
+    % simulation.
 
     global R % covariance matrix of motion model | shape 3X3
     global M % number of particles
     
+    % particle evolution based on odometry information
+    u = [(v * delta_t) .* cos(S(3, :));
+         v * delta_t .* sin(S(3, :));
+         repmat(omega * delta_t, 1, size(S, 2))];
+
     S_bar(1:3,:) = S(1:3,:) + u; % apply motion model
     S_bar(1:3,:) = S_bar(1:3,:) + randn(3, M) .* repmat(sqrt(diag(R)), 1, M); % diffusion
     S_bar(3, :) = mod(S_bar(3,:)+pi,2*pi) - pi; % keep angle in range
